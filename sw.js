c40a879943bff29a42965304203905e8742916a9
LVCarnevalli <LVCarnevalli@users.noreply.github.com>
2022-12-11 09:30:04 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0859f4bc964da57efcf5.js"
+    "url": "webpack-runtime-59352819ab7898ccf71b.js"
   },
   {
     "url": "framework-9b30aa3150f5f95f1944.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ac5619174455f830ed61c25897cd469e"
+    "revision": "57d2cd77698938981a94133fe45f9af8"
   },
   {
     "url": "manifest.webmanifest",
